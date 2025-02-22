d4849dbd706cd0b80750b459ce8575ad68626932
ArturoG1388 <arturo.garcia092708@gmail.com>
2025-02-22 12:09:33 -0500

    Coding

--- Assets/Geo_Quest/Scenes/Geocontroller.cs
+++ Assets/Geo_Quest/Scenes/Geocontroller.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Geocontroller : MonoBehaviour
 {
+     Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Scene_1";
     string String = "Hello";
     int burger = 3;
     // Start is called before the first frame update
@@ -12,13 +16,68 @@ public class Geocontroller : MonoBehaviour
         Debug.Log("Hello World");
         string String2 = "World";
         Debug.Log(String + String2);
+        rb = GetComponent<Rigidbody2D>();
+      
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         Debug.Log(burger);
         burger++;
         
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //transform.position += new Vector3(1, 0, 0);
+            //rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKeyDown (KeyCode.S))
+        {
+            //transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //transform.position += new Vector3(-1, 0, 0);
+            //rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+       
+        {
+            
+        }
+        float xinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xinput * speed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Runkiller");
+
+
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel); 
+                    break;
+                }
+                
+                
+
+
+        }
+
+
+
+
     }
 }
